f1589d7f7f58858a442e9ae230c5f597588b727f
woooyoung <axdsw121@gmail.com>
2024-07-09 14:49:46 +0900

    게시글 목록 조회 데이터를 DB로부터 가져오도록

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -1,9 +1,6 @@
 package org.koreait;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -14,7 +11,6 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int lastArticleId = 0;
-        List<Article> articles = new ArrayList<>();
 
         while (true) {
             System.out.print("명령어 > ");
@@ -31,11 +27,6 @@ public class Main {
                 String title = sc.nextLine();
                 System.out.print("내용 : ");
                 String body = sc.nextLine();
-
-                Article article = new Article(id, title, body);
-                articles.add(article);
-
-                System.out.println(article);
 
                 lastArticleId++;
                 System.out.println(id + "번 글이 작성되었습니다");
@@ -88,6 +79,64 @@ public class Main {
 
             } else if (cmd.equals("article list")) {
                 System.out.println("==목록==");
+
+                Connection conn = null;
+                PreparedStatement pstmt = null;
+                ResultSet rs = null;
+
+                List<Article> articles = new ArrayList<>();
+
+                try {
+                    Class.forName("org.mariadb.jdbc.Driver");
+                    String url = "jdbc:mariadb://127.0.0.1:3306/AM_JDBC_2024_07?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul";
+                    conn = DriverManager.getConnection(url, "root", "");
+                    System.out.println("연결 성공!");
+
+                    String sql = "SELECT * ";
+                    sql += "FROM article ";
+                    sql += "ORDER BY id DESC";
+
+                    System.out.println(sql);
+                    pstmt = conn.prepareStatement(sql);
+                    rs = pstmt.executeQuery(sql);
+
+                    while (rs.next()) {
+                        int id = rs.getInt("id");
+                        String regDate = rs.getString("regDate");
+                        String updateDate = rs.getString("updateDate");
+                        String title = rs.getString("title");
+                        String body = rs.getString("body");
+                        Article article = new Article(id, regDate, updateDate, title, body);
+                        articles.add(article);
+                    }
+
+                } catch (ClassNotFoundException e) {
+                    System.out.println("드라이버 로딩 실패" + e);
+                } catch (SQLException e) {
+                    System.out.println("에러 : " + e);
+                } finally {
+                    try {
+                        if (rs != null && !rs.isClosed()) {
+                            rs.close();
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        if (pstmt != null && !pstmt.isClosed()) {
+                            pstmt.close();
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        if (conn != null && !conn.isClosed()) {
+                            conn.close();
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
                 if (articles.size() == 0) {
                     System.out.println("게시글이 없습니다");
                     continue;
@@ -98,8 +147,6 @@ public class Main {
                 }
 
             }
-
-
         }
 
         System.out.println("==프로그램 종료==");
